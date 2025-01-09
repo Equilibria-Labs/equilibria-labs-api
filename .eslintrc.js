956732a382ad9ab57560bc3c41517d819fb6ec1f
Adam Morley <adam@adammorley.co.uk>
2025-01-09 14:54:27 +0000

    Disabled some lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
+const { off } = require('process');
+
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'next',
+    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': ['warn'],
     '@typescript-eslint/explicit-function-return-type': ['off'],
     '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          '{}': false,
-        },
-      },
-    ],
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
