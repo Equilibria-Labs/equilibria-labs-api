511dada7e2753f7aaf4e1e11215b3f238525d3bc
Adam Morley <adam@adammorley.co.uk>
2025-01-17 11:15:42 +0000

    Changed padding back

--- components/structure/Body.tsx
+++ components/structure/Body.tsx
@@ -1,3 +1,3 @@
 export default function Body({ children }: { children: React.ReactNode }) {
-  return <div className='flex flex-col gap-4 max-w-5xl p-5'>{children}</div>;
+  return <div className='flex flex-col gap-20 max-w-5xl p-5'>{children}</div>;
 }
