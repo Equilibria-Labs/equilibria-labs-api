46257033874419c55f80868c98ed162ab5342d1b
Equilibria-Labs <carmen@equilibrialabs.com>
2025-01-20 00:06:11 +0000

    compile issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [],
+  output: 'standalone',
+  distDir: '.next',
+  experimental: {
+    outputFileTracingRoot: require('path').join(__dirname, '../../'),
+  },
 };
 
 module.exports = nextConfig;
